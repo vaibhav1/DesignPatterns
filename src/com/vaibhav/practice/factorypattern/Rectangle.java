72d5c3cd89812fb52a87eb29509e74f7fe32d171
vaibhav1 <vaibhav2008in@gmail.com>
2016-09-30 13:01:48 +0530

    print message updated

--- src/com/vaibhav/practice/factorypattern/Rectangle.java
+++ src/com/vaibhav/practice/factorypattern/Rectangle.java
@@ -14,7 +14,7 @@ public class Rectangle implements Shape {
 	 */
 	@Override
 	public void draw() {
-		System.out.println("Rectangle is drawn");
+		System.out.println("Rectangle is drawn.");
 
 	}
 
--- src/com/vaibhav/practice/factorypattern/Triangle.java
+++ src/com/vaibhav/practice/factorypattern/Triangle.java
@@ -14,7 +14,7 @@ public class Triangle implements Shape {
 	 */
 	@Override
 	public void draw() {
-		System.out.println("Triangle is drawn");
+		System.out.println("Triangle is drawn.");
 
 	}
 
